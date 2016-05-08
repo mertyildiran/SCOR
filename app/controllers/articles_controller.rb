487fce6d3d6e048da0c5634b4d5f6e352d094995
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-05-08 03:30:02 +0300

    articles controller, articles/new action and view added.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,4 @@
+class ArticlesController < ApplicationController
+	def new
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :articles
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
