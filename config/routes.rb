3ae72122e405efc24a031af39ccb53054c47fd04
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-05-08 16:05:50 +0300

    Associating Models & Adding a Route for Comments

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  resources :articles do
+	  resources :comments
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
