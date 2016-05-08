472378ab84f4b74424616ee1dad00e073f25438d
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-05-08 16:21:41 +0300

    Basic Authentication.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,22 @@
 class CommentsController < ApplicationController
-  def create
-	@article = Article.find(params[:article_id])
-	@comment = @article.comments.create(comment_params)
-	redirect_to article_path(@article)
-  end
 
-  def destroy
-	@article = Article.find(params[:article_id])
-	@comment = @article.comments.find(params[:id])
-	@comment.destroy
-	redirect_to article_path(@article)
-  end
+	http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
-  private
+	def create
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.create(comment_params)
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
+		redirect_to article_path(@article)
+	end
+
+	private
 	def comment_params
-	  params.require(:comment).permit(:commenter, :body)
+		params.require(:comment).permit(:commenter, :body)
 	end
 end
