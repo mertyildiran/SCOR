999e9734fd270ba27f1125a8b7c9b2a403b23f6c
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-05-08 16:17:13 +0300

    Deleting Comments.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-	has_many :comments
-	validates :title, presence: true,
-					  length: { minimum: 5 }
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true,
+					length: { minimum: 5 }
 end
