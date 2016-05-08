ef44e968e5bedc08e15ac6c06b3a79466e309f53
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-05-08 16:01:57 +0300

    Generating Comment model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508003543) do
+ActiveRecord::Schema.define(version: 20160508130007) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20160508003543) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
