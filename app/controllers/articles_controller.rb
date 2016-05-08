ccc48ac8942c02164ead24bd233999055714aa8f
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-05-08 15:48:30 +0300

    Updating articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		  @article = Article.find(params[:id])
+	end
+
 	def create
 		@article = Article.new(article_params)
 
@@ -19,6 +23,16 @@ class ArticlesController < ApplicationController
 		else
 		  render 'new'
 		end
+	end
+
+	def update
+	  @article = Article.find(params[:id])
+
+	  if @article.update(article_params)
+		redirect_to @article
+	  else
+		render 'edit'
+	  end
 	end
 
 	private
