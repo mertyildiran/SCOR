31fc8f5fe3df63aef962bbe06acd95d52730f6c7
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-05-10 20:13:13 +0300

    Categories navigation bar added.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :nav_cat
+  def nav_cat
+	  @nav_cats = Category.all
+  end
 end
