52f46ee3616c0551b66817df36cd3652d9369de0
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2016-05-12 01:18:22 +0300

    README.md update.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Barebones of E-Commerce written on top of Ruby on Rails Framework
 
 ### Version
 
-0.3.6
+0.3.8
 
 ### Installation
 
@@ -15,7 +15,29 @@ rake db:seed
 rails server
 ```
 
+Visit: http://localhost:3000/
+
+### Admin Panel
+
+Create your first user using this form: http://localhost:3000/users/sign_up
+
+After that, set first user as admin:
+
+```Shell
+rails console
+u = User.first
+u.admin = true
+u.save
+exit
+```
+
+Make sure you are logged in as admin and visit: http://localhost:3000/admin
+
+*Note: If you are not logged in to system you will get an exception which is extremely normal.*
+
 ### Developer
+
+Write current state of database to seed:
 
 ```Shell
 rake db:seed:dump
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,11 @@
 RailsAdmin.config do |config|
 
+  RailsAdmin.config do |config|
+	config.authorize_with do
+	  redirect_to main_app.root_path unless current_user.admin?
+	end
+  end
+
   ### Popular gems integration
 
   ## == Devise ==
@@ -20,18 +26,18 @@ RailsAdmin.config do |config|
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
   config.actions do
-    dashboard                     # mandatory
-    index                         # mandatory
-    new
-    export
-    bulk_delete
-    show
-    edit
-    delete
-    show_in_app
+	dashboard                     # mandatory
+	index                         # mandatory
+	new
+	export
+	bulk_delete
+	show
+	edit
+	delete
+	show_in_app
 
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
+	## With an audit adapter, you can add:
+	# history_index
+	# history_show
   end
 end
